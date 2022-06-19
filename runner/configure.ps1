130aca205160369ec836e23150217ee8684a2f5c
Giovanni Bassi <giggio@giggio.net>
2022-06-19 05:41:48 -0300

    Only remove group if it exists

--- runner/configure.ps1
+++ runner/configure.ps1
@@ -12,12 +12,13 @@ if (!($env:REPO)) {
 $TOKEN = $env:TOKEN
 $REPO = $env:REPO
 $GROUP = $env:GROUP
-if (!($env:GROUP)) {
+if ($env:GROUP) {
+  Remove-Item Env:\GROUP
+} else {
   $GROUP = "Default"
 }
 Remove-Item Env:\TOKEN
 Remove-Item Env:\REPO
-Remove-Item Env:\GROUP
 . .\config.cmd --unattended --url https://github.com/$REPO --token $TOKEN --runnergroup $GROUP
 if ($LASTEXITCODE -ne 0) {
   Write-Error "Failed to configure GitHub Actions runner."
